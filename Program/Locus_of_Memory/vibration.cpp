20290568e939fef08f47d42b21919783673a8d01
nanba10 <251324@st.yoshida-g.ac.jp>
2026-01-28 15:00:47 +0900

    再掲載

---
+++ Program/Locus_of_Memory/vibration.cpp
@@ -0,0 +1,2 @@
+//test
+#include "vibration.h"
--- Program/Locus_of_Memory/vibration.h
+++ Program/Locus_of_Memory/vibration.h
@@ -1,1 +1,12 @@
-#pragma once
+//test
+
+#ifndef _VIBRATION_H        // このマクロ定義がされなかったら
+#define _VIBRATION_H        // 2重インクルード防止のマクロを定義する
+
+// プロトタイプ宣言
+void InitResult(void);
+void UninitResult(void);
+void UpdateResult(void);
+void DrawResult(void);
+
+#endif
