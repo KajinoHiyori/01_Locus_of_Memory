ebe609127500b51792812d5bf3212dca1aeaf6eb
Shuma Aizu <251301@st.yoshida-g.ac.jp>
2026-02-04 11:42:11 +0900

    effectがちらつく現象を修正

--- Program/Locus_of_Memory/effect.cpp
+++ Program/Locus_of_Memory/effect.cpp
@@ -198,7 +198,7 @@ void DrawEffect(void)
 			pDevice->SetTransform(D3DTS_WORLD, &g_aEffect[nCntEffect].g_mtxWorldPolygon);
 
 			//頂点バッファをデータストリームに設定
-			pDevice->SetStreamSource(0, g_pVtxBuffPolygon, nCntEffect * 4, sizeof(VERTEX_3D));
+			pDevice->SetStreamSource(0, g_pVtxBuffPolygon, 0, sizeof(VERTEX_3D));
 
 			//頂点フォーマットの設定
 			pDevice->SetFVF(FVF_VERTEX_3D);
