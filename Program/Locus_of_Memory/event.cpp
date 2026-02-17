a0e006f2b48af759293e314859f1387a08aaae6e
Shuma Aizu <251301@st.yoshida-g.ac.jp>
2026-02-17 10:53:17 +0900

    パーティクル時間の細かい調整

--- Program/Locus_of_Memory/event.cpp
+++ Program/Locus_of_Memory/event.cpp
@@ -61,7 +61,7 @@ bool SetMagicEvent001(MAGICTYPE type, int nIdx)
 		// ”RÄƒCƒxƒ“ƒg
 	case MAGICTYPE_COMBUSTION:
 		pParentObject->EventType = EVENTTYPE_001_1;
-		SetParticle(pParentObject->pos, 300, PARTICLETYPE_COMBUSTION);
+		SetParticle(pParentObject->pos, 150, PARTICLETYPE_COMBUSTION);
 		SetMotion(&pParentObject->motion, pParentObject->pModelData, MOTIONTYPE_MOVE, false, true, 10);
 		return true;
 
