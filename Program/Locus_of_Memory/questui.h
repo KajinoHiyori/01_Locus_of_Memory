0ecb6dd873eccded31a55014d929b5489cf5c85a
KajinoHiyori <251312@st.yoshida-g.ac.jp>
2026-03-06 17:38:36 +0900

    クエストUIはもう少し人がいるときに決めます

--- Program/Locus_of_Memory/questui.h
+++ Program/Locus_of_Memory/questui.h
@@ -1,1 +1,16 @@
-#pragma once
+//========================================================
+// 
+// クエストのUI表示処理[questui.h]
+// Author : KajinoHiyori
+// 
+//========================================================
+#ifndef _QUESTUI_H_
+#define	_QUESTUI_H_
+
+// プロトタイプ宣言
+void InitQuestUI(void);
+void UninitQuestUI(void);
+void UpdateQuestUI(void);
+void DrawQuestUI(void);
+
+#endif
