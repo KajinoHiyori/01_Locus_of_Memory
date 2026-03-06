75de425dd9278567bfa81712d38c6d3ed20b1396
Kairi Manabe <251326@st.yoshida-g.ac.jp>
2026-03-06 14:52:31 +0900

    magiceffectを更新

--- Program/Locus_of_Memory/magiceffect.h
+++ Program/Locus_of_Memory/magiceffect.h
@@ -4,28 +4,10 @@
 typedef enum
 {
 	MAGICEF_TYPE_NONE = -1,
-	//浮遊
-	MAGICEF_TYPE_LEVITATION,
-	//燃焼
-	MAGICEF_TYPE_COMBUSTION,
-	//洪水、氾濫
-	MAGICEF_TYPE_FLOOD,
-	//フラッシュ
-	MAGICEF_TYPE_FLASH,
-	//火球
-	MAGICEF_TYPE_FIREBALL,
-	//太陽の動きを遅延する
-	MAGICEF_TYPE_SUNSETDELAY,
-	//雨乞い
-	MAGICEF_TYPE_RAINPRAY,
-	//凍結
-	MAGICEF_TYPE_FREEZE,
-	//成長(植物など)
-	MAGICEF_TYPE_GROWTH,
-	//加速
-	MAGICEF_TYPE_ACCELERATION,
-	//時間の巻き戻し(回帰)
-	MAGICEF_TYPE_TIMEREVERT,
+	//ギミック用
+	MAGICEF_TYPE_GIMMICK,
+	//落ちている魔法用
+	MAGICEF_TYPE_MAGIC,
 
 	MAGICEF_TYPE_MAX
 }MAGICEF_TYPE;
@@ -35,6 +17,7 @@ void InitMagicEffect(void);
 void UninitMagicEffect(void);
 void UpdateMagicEffect(void);
 void DrawMagicEffect(void);
-void SetMagicEffect(D3DXVECTOR3 pos, int nLife, MAGICEF_TYPE type);
+void SetMagicEffect(D3DXVECTOR3 pos, MAGICEF_TYPE type);
+void StopMagicEffect(int nIdx);
 #endif 
 
